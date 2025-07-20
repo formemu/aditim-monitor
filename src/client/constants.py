06dfdc649800c4a3b6cc4c3bb64874962e3cd6e9
formemu <for.memu@gmail.com>
2025-07-20 17:51:53 +0300

    Исправление проблем с UI файлами и ресурсами
    - Восстановлен корректный main_window.ui файл
    - Создан отсутствующий dialogtask.ui
    - Централизованы пути к ресурсам в constants.py
    - Добавлены utility функции для работы с путями
    - Временно отключены действия меню для упрощенного UI
    - Клиент теперь запускается успешно

--- src/client/constants.py
+++ src/client/constants.py
@@ -3,6 +3,10 @@ Constants for ADITIM Monitor Client
 """
 
 import os
+from pathlib import Path
+
+# Базовый путь к client директории
+CLIENT_BASE_PATH = Path(__file__).parent
 
 API_BASE_URL = os.getenv('ADITIM_API_URL', 'http://127.0.0.1:8000')
 API_TIMEOUT = int(os.getenv('ADITIM_API_TIMEOUT', '30'))
@@ -38,6 +42,31 @@ PATHS = {
     "IMAGES": "src/client/resources/images/"
 }
 
+# UI Paths (относительные пути от client директории)
+UI_PATHS = {
+    "MAIN_WINDOW": "ui/main_window.ui",
+    "HOME_PAGE": "ui/home_page.ui", 
+    "PROFILES_CONTENT": "ui/window_profiles.ui",
+    "DIALOG_CREATE_PROFILE": "ui/dialog_create_profile.ui",
+    "DIALOG_TASK": "ui/dialogtask.ui"
+}
+
+# Icon Paths (относительные пути от client директории)
+ICON_PATHS = {
+    "ADD": "resources/icons/icon_add.svg",
+    "EDIT": "resources/icons/icon_edit.svg",
+    "DELETE": "resources/icons/icon_delete.svg", 
+    "LOGO": "resources/icons/aditim_logo.ico",
+    "LOGO_PNG": "resources/icons/aditim_logo.png",
+    "LOGO_JPG": "resources/icons/aditim_logo2.jpg"
+}
+
+# Style Paths (относительные пути от client директории)
+STYLE_PATHS = {
+    "MAIN": "resources/styles/main_template.qss",
+    "DIALOGS": "resources/styles/dialogs_template.qss"
+}
+
 # Task Statuses
 TASK_STATUSES = [
     {"id": 1, "name": "Новая"},
@@ -60,3 +89,21 @@ PROFILE_EQUIPMENT = [
     "усреднитель",
     "кондуктор"
 ]
+
+# Utility functions for path resolution
+def get_ui_path(ui_name: str) -> str:
+    """Получить абсолютный путь к UI файлу"""
+    return str(CLIENT_BASE_PATH / UI_PATHS[ui_name])
+
+def get_icon_path(icon_name: str) -> str:
+    """Получить абсолютный путь к иконке"""
+    return str(CLIENT_BASE_PATH / ICON_PATHS[icon_name])
+
+def get_style_path(style_name: str) -> str:
+    """Получить абсолютный путь к файлу стилей"""
+    return str(CLIENT_BASE_PATH / STYLE_PATHS[style_name])
+
+# Создаем словари с абсолютными путями для обратной совместимости
+UI_PATHS_ABS = {key: get_ui_path(key) for key in UI_PATHS.keys()}
+ICON_PATHS_ABS = {key: get_icon_path(key) for key in ICON_PATHS.keys()}  
+STYLE_PATHS_ABS = {key: get_style_path(key) for key in STYLE_PATHS.keys()}
--- src/client/resources.py
+++ src/client/resources.py
@@ -2,32 +2,7 @@
 Единый файл ресурсов для ADITIM Monitor
 """
 
-import os
-from pathlib import Path
+from .constants import UI_PATHS_ABS as UI_PATHS, ICON_PATHS_ABS as ICON_PATHS, STYLE_PATHS_ABS as STYLE_PATHS
 
-# Базовый путь к ресурсам
-BASE_PATH = Path(__file__).parent
-
-# Пути к UI файлам
-UI_PATHS = {
-    "MAIN_WINDOW": str(BASE_PATH / "ui" / "main_window.ui"),
-    "HOME_PAGE": str(BASE_PATH / "ui" / "home_page.ui"),
-    "PROFILES_CONTENT": str(BASE_PATH / "ui" / "window_profiles.ui"),
-    "DIALOG_CREATE_PROFILE": str(BASE_PATH / "ui" / "dialog_create_profile.ui"),
-}
-
-# Пути к иконкам
-ICON_PATHS = {
-    "ADD": str(BASE_PATH / "resources" / "icons" / "icon_add.svg"),
-    "EDIT": str(BASE_PATH / "resources" / "icons" / "icon_edit.svg"), 
-    "DELETE": str(BASE_PATH / "resources" / "icons" / "icon_delete.svg"),
-    "LOGO": str(BASE_PATH / "resources" / "icons" / "aditim_logo.ico"),
-    "LOGO_PNG": str(BASE_PATH / "resources" / "icons" / "aditim_logo.png"),
-    "LOGO_JPG": str(BASE_PATH / "resources" / "icons" / "aditim_logo2.jpg"),
-}
-
-# Пути к стилям
-STYLE_PATHS = {
-    "MAIN": str(BASE_PATH / "resources" / "styles" / "main_template.qss"),
-    "DIALOGS": str(BASE_PATH / "resources" / "styles" / "dialogs_template.qss"),
-}
+# Экспортируем пути для обратной совместимости
+__all__ = ['UI_PATHS', 'ICON_PATHS', 'STYLE_PATHS']
